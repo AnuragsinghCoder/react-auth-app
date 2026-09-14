d243211498c610d8b9b7889c02d6d0fe0af27844
AnuragsinghCoder <www.saury@gmail.com>
2026-09-15 01:39:43 +0530

    configured users api in home file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
     useEffect(() => {
         const fetchUsers = async () => {
             try {
-                const response = await fetch("http://localhost:5000/users/");
+                const response = await fetch("https://react-auth-app-backend.vercel.app/users/");
 
                 if (!response.ok) {
                     throw new Error("Failed to fetch users");
